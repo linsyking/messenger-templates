b1eb3c1bbca3e0afbb343b21a49ac58ef605759a
linsy king <linsy_king@sjtu.edu.cn>
2023-05-27 21:02:35 +0800

    fix: syntax

--- sceneproto/scene/Export.elm
+++ sceneproto/scene/Export.elm
@@ -14,9 +14,9 @@ Normally you don't need to change this file.
 
 import Lib.Env.Env exposing (Env)
 import Lib.Scene.Base exposing (Scene, SceneInitData(..), SceneTMsg(..))
-import SceneProtos.$0.Common exposing (Model)
+import SceneProtos.$0.Common exposing (Model, initModel)
 import SceneProtos.$0.LayerInit exposing ($0Init)
-import SceneProtos.$0.Model exposing (initModel, updateModel, viewModel)
+import SceneProtos.$0.Model exposing (updateModel, viewModel)
 
 
 {-| Data
