a0ae7e75aaed3b07aa3043c17c83ef86c296b1f1
linsy king <linsy_king@sjtu.edu.cn>
2023-06-03 23:36:34 +0800

    fix: gc name

--- sceneproto/gamecomponent/Sample/Model.elm
+++ sceneproto/gamecomponent/Sample/Model.elm
@@ -8,7 +8,7 @@ module SceneProtos.$0.GameComponents.$1.Model exposing (initModel, updateModel, 
 @docs initModel, updateModel, viewModel
 
 -}
-import Canvas exposing (Renderable, empty)
+import Canvas exposing (Renderable)
 import Lib.Env.Env exposing (Env, EnvC)
 import SceneProtos.$0.GameComponent.Base exposing (Data, GameComponentInitData(..), GameComponentMsg, GameComponentTarget, nullData)
 import SceneProtos.$0.LayerBase exposing (CommonData)
@@ -22,7 +22,7 @@ Initialize the model. It should update the id.
 initModel : Env -> GameComponentInitData -> Data
 initModel _ initData =
     case initData of
-        GCIdData _ (GCButInitData _) ->
+        GCIdData _ (GC$1InitData _) ->
             -- Change this to initialize your GC
             nullData
 
@@ -44,9 +44,7 @@ updateModel env _ d =
 
 Change this to your own component view function.
 
-If there is no view function, return Nothing.
-
 -}
-viewModel : EnvC CommonData -> Data -> Renderable
+viewModel : EnvC CommonData -> Data -> List ( Renderable, Int )
 viewModel _ _ =
-    empty
+    []
