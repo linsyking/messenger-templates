8dd5d54c7a1baa1ab7bba0a4cd4c8f118d1b7c74
linsyking <kxiang@umich.edu>
2024-05-14 15:42:56 -0400

    feat: sceneproto

---
+++ sceneproto/LayerBase.elm
@@ -0,0 +1,36 @@
+module Scenes.$0.LayerBase exposing
+    ( LayerTarget
+    , SceneCommonData
+    , LayerMsg(..)
+    )
+
+{-|
+
+
+# LayerBase
+
+Basic data for the layers.
+
+@docs LayerTarget
+@docs SceneCommonData
+@docs LayerMsg
+
+-}
+
+
+{-| Layer target type
+-}
+type alias LayerTarget =
+    String
+
+
+{-| Common data
+-}
+type alias SceneCommonData =
+    {}
+
+
+{-| General message for layers
+-}
+type LayerMsg
+    = NullLayerMsg
---
+++ sceneproto/Layered/Model.elm
@@ -0,0 +1,47 @@
+module Scenes.$0.Model exposing (scene)
+
+{-| Scene configuration module
+
+@docs scene
+
+-}
+
+import Lib.Base exposing (SceneMsg)
+import Lib.UserData exposing (UserData)
+import Messenger.Base exposing (Env, addCommonData)
+import Messenger.Scene.LayeredScene exposing (LayeredSceneInit, LayeredSceneSettingsFunc, genLayeredScene)
+import Messenger.Scene.Scene exposing (SceneStorage)
+import Scenes.$0.LayerBase exposing (..)
+
+
+commonDataInit : Env () UserData -> Maybe SceneMsg -> SceneCommonData
+commonDataInit _ _ =
+    {}
+
+
+init : LayeredSceneInit SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+init env msg =
+    let
+        cd =
+            commonDataInit env msg
+
+        envcd =
+            addCommonData cd env
+    in
+    { renderSettings = []
+    , commonData = cd
+    , layers =
+        []
+    }
+
+
+settings : LayeredSceneSettingsFunc SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+settings _ _ _ =
+    []
+
+
+{-| Scene generator
+-}
+scene : SceneStorage UserData SceneMsg
+scene =
+    genLayeredScene init settings
