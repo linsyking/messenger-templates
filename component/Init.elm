83cdb16e5a41f49c3393a927d385c29e55629717
linsyking <kxiang@umich.edu>
2024-05-16 11:03:23 -0400

    feat: init

---
+++ component/Init.elm
@@ -0,0 +1,16 @@
+module $0.$1.$2.$3.Init exposing (InitData)
+
+{-|
+
+
+# Init module
+
+@docs InitData
+
+-}
+
+
+{-| The data used to initialize the scene
+-}
+type alias InitData =
+    {}
---
+++ scene/Init.elm
@@ -0,0 +1,16 @@
+module $0.$1.Init exposing (InitData)
+
+{-|
+
+
+# Init module
+
+@docs InitData
+
+-}
+
+
+{-| The data used to initialize the scene
+-}
+type alias InitData =
+    {}
